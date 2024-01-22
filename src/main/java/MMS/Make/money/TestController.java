d5706bbf41c6fd5d97df46185ecaa6cf74f51f83
baeyunhyeok <yunhyeok0928@gmail.com>
2024-01-22 22:21:55 +0900

    home Test Commit

--- src/main/java/MMS/Make/money/TestController.java
+++ src/main/java/MMS/Make/money/TestController.java
@@ -2,8 +2,14 @@ package MMS.Make.money;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
 
+    @RequestMapping("/")
+    public String home() {
+        return "안녕하세요.";
+    }
 }
