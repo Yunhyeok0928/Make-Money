ecc407a2caf61c744bf90275985cb78344264ce5
baeyunhyeok <yunhyeok0928@gmail.com>
2024-01-22 20:10:18 +0900

    Test Commit

---
+++ src/main/java/MMS/Make/money/MakeMoneyApplication.java
@@ -0,0 +1,13 @@
+package MMS.Make.money;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MMS/Make/money/MakeMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package MMS.Make.money;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
